d4a7f68200e8ca3592cec2ac4fc1fc3d31f89d83
Valentina Otočan <valentinaotocan@gmail.com>
2023-02-19 21:54:15 +0100

    className and styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,39 +10,43 @@ interface Movies {
 function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState<Movies[]>([]);
-  
+
   async function handleFetch() {
     const response = await fetch(
       `https://www.omdbapi.com/?s=${query}&apikey=5d763802`
     );
     const data = await response.json();
     setMovies(data.Search);
-    console.log(data.Search)
+    console.log(data.Search);
   }
 
   return (
-    <section>
+    <section className="movie">
       <h1>Enter name of a movie:</h1>
       {/* <form> */}
-        <input
-          type="text"
-          value={query}
-          onChange={(event) => {
-            setQuery(event.target.value);
-          }}
-        />
-        <button onClick={handleFetch}>Search</button>
+      <input
+        type="text"
+        value={query}
+        onChange={(event) => {
+          setQuery(event.target.value);
+        }}
+      />
+      <button onClick={handleFetch}>Search</button>
       {/* </form> */}
-      <div>
+      <div className="movie__container">
         {movies.map((movie) => {
           return (
-            <div key={movie.imbdID}>
-              <img src={movie.Poster} alt="" />
-              <h2>{movie.Title}</h2>
-              <p>{movie.Year}</p>
+            <div key={movie.imbdID} className="movie__container__content" style={{backgroundImage: `url(${movie.Poster})`}}>
+              <div
+                className="movie__container__content__text"
+                
+              >
+                  <h2>{movie.Title}</h2>
+                  <p>{movie.Year}</p>
+              </div>
             </div>
-          )
-          })}
+          );
+        })}
       </div>
     </section>
   );
